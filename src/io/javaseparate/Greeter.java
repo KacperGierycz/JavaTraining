02ec82c380fa8651a8cc5b381f296b332ede7721
Kacper <kacpergierycz@gmail.com>
2020-06-25 08:35:43 +0200

    first commit

---
+++ src/io/javaseparate/Greeter.java
@@ -0,0 +1,15 @@
+package io.javaseparate;
+
+public class Greeter {
+
+	public void greet() {
+		System.out.println("hello world");
+	}
+	
+	public static void main(String[] args) {
+		Greeter greeter=new Greeter();
+		greeter.greet();
+
+	}
+
+}
